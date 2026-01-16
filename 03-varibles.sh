f155a618fd1b4b03424ffb368f04525109825da1
vinay raj <vinaymudhiraj918@gmail.com>
2026-01-16 19:23:41 +0530

    some message

---
+++ 03-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1="Ramesh"
+Person2="Suresh"
+
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hi $Person1, I am fine. How about you?"
+echo "$Person1:: I am doing well, thanks for asking!"   
