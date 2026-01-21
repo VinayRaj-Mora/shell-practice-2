f59c3b54894308ac9f18ea1e966f278d1d5ba4cd
vinay raj <vinaymudhiraj918@gmail.com>
2026-01-21 15:25:26 +0530

    Varables

---
+++ 04-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hi $Person2,How are you?"
+echo "$Person2:: Hi $Person1, I am fine. How about you?"
+echo "$Person1:: I am doing well, thanks for asking!"
