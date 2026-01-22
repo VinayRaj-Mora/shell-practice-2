4310cd28a12c321ddc9c7b7bbf38a7b43db4e343
vinay raj <vinaymudhiraj918@gmail.com>
2026-01-22 18:38:08 +0530

    Varables-1

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -3,3 +3,6 @@
 echo "All varibles passed to this script are: $@"
 echo "All varibles passed to this script are: $*"
 echo "Total number of varibles passed to this script are: $#"
+echo "Script name is: $0"
+echo "Process ID of current script is: $$"
+echo "Last command exit status is: $?"  
